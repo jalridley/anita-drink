f1b7e2b040652f3ec4deff64a8a38f36bc6bcfee
Jal Ridley <72085091+jalridley@users.noreply.github.com>
2024-10-25 23:41:29 +0200

    images to hero

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,7 +1,29 @@
+import Image from 'next/image';
+
 const Hero = () => {
   return (
-    <section className="relative flex h-screen w-full justify-center bg-black">
-      <div>Hero/Image/Text</div>
+    <section className="flex h-screen w-full items-center justify-between bg-black px-4 lg:px-6">
+      <Image
+        className="h-[1000px] w-auto bg-cyan-400"
+        alt="black and white stencil of Anita Drink's face"
+        src={'/Anita_Drink_Stencil_transp.png'}
+        width={1249}
+        height={1707}
+      />
+      <Image
+        className="h-[1000px] w-auto"
+        alt="black and white stencil of Anita Drink's face"
+        src={'/Anita_Drink_Stencil.jpg'}
+        width={1249}
+        height={1707}
+      />
+      <Image
+        className="h-[1000px] w-auto bg-lime-400"
+        alt="black and white stencil of Anita Drink's face"
+        src={'/Anita_Drink_Stencil_transp.png'}
+        width={1249}
+        height={1707}
+      />
     </section>
   );
 };
