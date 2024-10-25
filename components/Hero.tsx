939d1baeaa57a4856b962ae42081a1fcf7aa7f2f
Jal Ridley <72085091+jalridley@users.noreply.github.com>
2024-10-26 00:38:30 +0200

    delete padding from hero

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -2,7 +2,7 @@ import Image from 'next/image';
 
 const Hero = () => {
   return (
-    <section className="flex h-[calc(100vh-56px)] w-full items-center justify-between bg-black px-4 lg:px-6">
+    <section className="flex h-[calc(100vh-56px)] w-full items-center justify-between bg-black">
       <Image
         className="h-[1000px] w-auto bg-cyan-400"
         alt="black and white stencil of Anita Drink's face"
