0ede9e68ddcd3a6edc5b1d50ac4a37fa9b8d59c2
Jal Ridley <72085091+jalridley@users.noreply.github.com>
2024-10-26 00:38:58 +0200

    padding to main, mixcloud iframes

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,60 @@ import Hero from '@/components/Hero';
 
 export default function Home() {
   return (
-    <main className="pt-14">
+    <main className="px-4 pt-14 lg:px-6">
       <Hero />
+      <div>
+        <iframe
+          width="100%"
+          height="120"
+          src="https://player-widget.mixcloud.com/widget/iframe/?hide_cover=1&light=1&feed=%2Fanitadrink%2Fanita-drink-live-mix-at-lunch-box-candy-berlin%2F"
+        ></iframe>
+        <iframe
+          width="100%"
+          height="120"
+          src="https://player-widget.mixcloud.com/widget/iframe/?hide_cover=1&light=1&feed=%2Fanitadrink%2Fanita-drink-built-for-speed-mix%2F"
+        ></iframe>
+        <iframe
+          width="100%"
+          height="120"
+          src="https://player-widget.mixcloud.com/widget/iframe/?hide_cover=1&light=1&feed=%2Fanitadrink%2Fanita-drink-eye-am-the-dj-mixtape%2F"
+        ></iframe>
+        <iframe
+          width="100%"
+          height="120"
+          src="https://player-widget.mixcloud.com/widget/iframe/?hide_cover=1&light=1&feed=%2Fanitadrink%2Fanita-drink-presents-walking-after-midnight-mix%2F"
+        ></iframe>
+        <iframe
+          width="100%"
+          height="120"
+          src="https://player-widget.mixcloud.com/widget/iframe/?hide_cover=1&light=1&feed=%2Fanitadrink%2Fanita-drink-presents-16th-valencia-mix%2F"
+        ></iframe>
+        <iframe
+          width="100%"
+          height="120"
+          src="https://player-widget.mixcloud.com/widget/iframe/?hide_cover=1&light=1&feed=%2Fanitadrink%2Fanita-drink-presents-berlin-mambo-part-1%2F"
+        ></iframe>
+        <iframe
+          width="100%"
+          height="120"
+          src="https://player-widget.mixcloud.com/widget/iframe/?hide_cover=1&light=1&feed=%2Fanitadrink%2Fanita-drink-presents-berlin-mambo-part-2%2F"
+        ></iframe>
+        <iframe
+          width="100%"
+          height="120"
+          src="https://player-widget.mixcloud.com/widget/iframe/?hide_cover=1&light=1&feed=%2Fanitadrink%2Fanita-drink-presents-soul-train%2F"
+        ></iframe>
+        <iframe
+          width="100%"
+          height="120"
+          src="https://player-widget.mixcloud.com/widget/iframe/?hide_cover=1&light=1&feed=%2Fanitadrink%2Fanita-drink-presents-boys-who-are-girls-mix%2F"
+        ></iframe>
+        <iframe
+          width="100%"
+          height="120"
+          src="https://player-widget.mixcloud.com/widget/iframe/?hide_cover=1&light=1&feed=%2Fanitadrink%2Fanita-drink-presents-house-of-exit-tea-dance-feat-lotsa-drink%2F"
+        ></iframe>
+      </div>
     </main>
   );
 }
