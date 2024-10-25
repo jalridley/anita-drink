ae24adea59ce3e5213e36be13008a106199d7780
Jal Ridley <72085091+jalridley@users.noreply.github.com>
2024-10-25 20:48:23 +0200

    Hero component

---
+++ components/Hero.tsx
@@ -0,0 +1,9 @@
+const Hero = () => {
+  return (
+    <section className="relative flex h-screen w-full justify-center bg-black">
+      <div>Hero/Image/Text</div>
+    </section>
+  );
+};
+
+export default Hero;
