067b1013ed208e1eb86a4a6e50622d4df6cbbe3b
Jal Ridley <72085091+jalridley@users.noreply.github.com>
2024-10-26 14:34:05 +0200

    hero grid

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -2,23 +2,23 @@ import Image from 'next/image';
 
 const Hero = () => {
   return (
-    <section className="flex h-[calc(100vh-56px)] w-full items-center justify-between bg-black">
+    <section className="grid h-[calc(100vh-56px)] w-full grid-cols-3 gap-2 bg-black py-4 md:gap-4 lg:items-center lg:gap-12">
       <Image
-        className="h-[1000px] w-auto bg-cyan-400"
+        className="w-full bg-cyan-400"
         alt="black and white stencil of Anita Drink's face"
         src={'/Anita_Drink_Stencil_transp.png'}
         width={1249}
         height={1707}
       />
       <Image
-        className="h-[1000px] w-auto"
+        className="w-full"
         alt="black and white stencil of Anita Drink's face"
         src={'/Anita_Drink_Stencil.jpg'}
         width={1249}
         height={1707}
       />
       <Image
-        className="h-[1000px] w-auto bg-lime-400"
+        className="w-full bg-lime-400"
         alt="black and white stencil of Anita Drink's face"
         src={'/Anita_Drink_Stencil_transp.png'}
         width={1249}
