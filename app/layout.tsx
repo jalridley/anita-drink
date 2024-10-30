eb3de703274c10044d8d6cae3c4ada004630b762
Jal Ridley <72085091+jalridley@users.noreply.github.com>
2024-10-30 18:31:06 +0100

    social media icons to footer

--- app/layout.tsx
+++ app/layout.tsx
@@ -72,29 +72,31 @@ export default function RootLayout({
             </nav>
           </header>
           <div className="pt-14">{children}</div>
-          <footer className="md:text-md -top-14 mt-14 flex h-8 w-full shrink-0 flex-col items-center justify-between gap-2 bg-black px-4 py-6 text-sm font-extrabold text-white sm:flex-row lg:px-6">
-            <p className="shrink-0">© 2024 Anita Drink all rights reserved.</p>
-            <nav className="flex gap-4 sm:ml-auto sm:gap-6">
+          <footer className="-top-14 mt-14 flex h-8 w-full shrink-0 items-center justify-between gap-2 bg-black px-4 py-6 text-xs font-extrabold text-white sm:flex-row md:text-sm lg:px-6">
+            <p className="hidden shrink-0 md:block">
+              ©2024 Anita Drink. All rights reserved.
+            </p>
+            <nav className="flex h-8 w-full justify-center gap-6 sm:gap-8 md:justify-end">
               <Link
                 href={'https://www.instagram.com/anitadrinklipstick/'}
                 target="_blank"
               >
-                <FaInstagram />
+                <FaInstagram className="h-full w-full" />
               </Link>
               <Link
                 href={'https://www.facebook.com/djanitadrink/'}
                 target="_blank"
               >
-                <FaFacebook />
+                <FaFacebook className="h-full w-full" />
               </Link>
               <Link href={'https://eatlipstick.bandcamp.com/'} target="_blank">
-                <FaBandcamp />
+                <FaBandcamp className="h-full w-full" />
               </Link>
               <Link
                 href={'https://www.mixcloud.com/anitadrink/'}
                 target="_blank"
               >
-                <FaMixcloud />
+                <FaMixcloud className="h-full w-full" />
               </Link>
             </nav>
           </footer>
