2d7033cf9eb950022831f5ff21f87040b4e81b62
Jal Ridley <72085091+jalridley@users.noreply.github.com>
2024-12-10 16:09:46 +0100

    removed repeated class

--- components/Marquee.tsx
+++ components/Marquee.tsx
@@ -1,12 +1,12 @@
 const Marquee = () => {
   return (
-    <div className="relative flex h-1/4 items-center overflow-hidden text-[6rem] font-extrabold leading-none text-pink-500 opacity-90 md:text-[6rem] lg:text-[10rem] xl:text-[16rem]">
+    <div className="relative flex h-1/4 items-center overflow-hidden text-[6rem] font-extrabold leading-none text-pink-500 opacity-90 lg:text-[10rem] xl:text-[16rem]">
       <div className="animate-marquee whitespace-nowrap">
         <span className="mx-4">ANITA DRINK&nbsp;</span>
         <span className="mx-4">ANITA DRINK&nbsp;</span>
       </div>
 
-      <div className="animate-marquee2 absolute whitespace-nowrap">
+      <div className="absolute animate-marquee2 whitespace-nowrap">
         <span className="mx-4">ANITA DRINK&nbsp;</span>
         <span className="mx-4">ANITA DRINK&nbsp;</span>
       </div>
