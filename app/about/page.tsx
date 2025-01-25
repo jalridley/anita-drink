9750893f001566deb38c3eb0da860160b175ed08
Jal Ridley <72085091+jalridley@users.noreply.github.com>
2025-01-25 15:21:14 +0100

    updated with new bio

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -5,20 +5,20 @@ const AboutPage = () => {
     {
       title: 'Anita Drink',
       subtitle: 'WARNING!',
-      text: 'This DJ Plays music like exploding make-up! Full of color!\nCrossing all genres and genders!\nA passionate selector of over 37 years!',
+      text: "Anita Drink is the most Juan Of a Kind DJ of all time!\n\nAnita Drink began their career as a singer and club DJ in the early 80s and has been a full-time DJ ever since. Their love of music that began at birth has a back bone fusion of Punk's rebellious spirit and Disco's infectious rhythms. \n\nStarting out in clubs and radio they have developed a following around the world known for genre bending sets mixed with a tongue in cheek finesse only Anita could pull off!",
       alt: 'Anita putting makeup powder on face',
       imageSrc: '/anita-makeup.jpg',
     },
     {
       subtitle: 'VINYL QUEEN',
-      text: 'Building a loyal dance floor following around the world, Anita Drink is a performer whose love of all vinyl sets is well known.\nWhen she is not on stage as vocalist in her band Eat Lipstick, she is knee deep in the grooves that keep her busy playing her favorite clubs and parties.\nShe is the brain child of Berlin fashion designer Juan Chamié nee´ John Chamié. DJ career in the early 80´s bringing house and imported dance music to the night clubs of Denver, Colorado.Building a legacy that grew into its own historic dance culture helping to nurture the West Coast sound.',
-      alt: 'Anita posing pink party streamers',
+      text: 'Their love of deejaying with vinyl is matched only by their reputation in managing energy and telling a story on the dance floor.\n\nThe story started as Juan chooses the DJ name John Chamié to start throwing parties and events melting fashion and his love of music into a passionate mix along with his friends they laid the foundation of a club scene that is still in existence today. As a radio and record pool member, he brought new music to the airwaves and exposed and opened doors for programming new styles of music and breaking new acts in a traditional radio market.\n\nDuring this era, he began nurturing talent while working as producer and creating electronic music, with his co-founders of Terraform records. Formed in the early 90s while working as a producer and label artist Agent Babylon The label grew to have an impressive discography of ambient, trip hop, house, drum & bass and electro artists.',
+      alt: 'Anita in pink pulling at hair',
       imageSrc: '/anita-pink.jpg',
     },
     {
       subtitle: 'BERLIN',
-      text: 'As a founding member of Terraform records he helped nurture and grow the music from this thriving scene that lives to this day.\nReleasing his own productions as Drop Culture, The Vandal.',
-      alt: 'Anita posing pink party streamers',
+      text: 'Their passionate career has grown with them from Denver to San Francisco and now based in Berlin. As a fashion designer and owner of the Boutique EXIT Berlin where they work developing their brand based on their love of music and punk soul roots.\n\nAnita Drink is also the singer of the infamous band Eat Lipstick formed in 2008 their explosive shows gained a reputation leading up to their second LP New Wig No Rules! Produced by Peaches it solidified their place in Berlin music herstory.',
+      alt: 'Anita in a red dress',
       imageSrc: '/anita-red-dress.jpg',
     },
   ];
