22f16c2a489c2ab85ed3e1fafc8b6bbce0efab1d
Jal Ridley <72085091+jalridley@users.noreply.github.com>
2025-02-08 17:23:49 +0100

    fix(Footer): update copyright year to 2025

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -8,9 +8,9 @@ import {
 
 const Footer = () => {
   return (
-    <footer className="-top-14 mt-14 flex h-8 w-full shrink-0 items-center justify-between gap-2 bg-black px-4 py-6 text-xs font-extrabold text-white sm:flex-row md:text-sm lg:px-6">
+    <footer className="-top-14 flex h-8 w-full shrink-0 items-center justify-between gap-2 bg-black px-4 py-6 text-xs font-extrabold text-white sm:flex-row md:text-sm lg:px-6">
       <p className="hidden shrink-0 md:block">
-        ©2024 Anita Drink. All rights reserved.
+        ©2025 Anita Drink. All rights reserved.
       </p>
       <nav className="flex h-8 w-full justify-center gap-6 sm:gap-8 md:justify-end">
         <Link href={'https://www.mixcloud.com/anitadrink/'} target="_blank">
