6496e26c6ee07ab20961fd885f0018b683b80b61
Jal Ridley <72085091+jalridley@users.noreply.github.com>
2025-04-06 20:58:10 +0200

    fix(home): update dj set titles with additional context in Home page content

--- app/page.tsx
+++ app/page.tsx
@@ -31,18 +31,18 @@ export default function Home() {
       imageSrc: '/anita-stencil-square.webp',
       playerSrc:
         'https://player-widget.mixcloud.com/widget/iframe/?hide_cover=1&light=1&feed=%2Fanitadrink%2Fanita-drink-built-for-speed-mix%2F',
-      date: '05.05.20',
+      date: '05.05.20 Delirium',
       description:
-        "This mix is quintessential Anita Drink at her best ! Recorded for San Francisco's legendary Built for Speed party at Delirium in the Mission at 16 & Valencia. This Thursday weekly ran for 11 years! featuring tracks inspired and played by her co-founders DJ sisters Platform Whore & Fashion Hooker. Get ready to strap in for a wild ride! Featuring tracks by The Monkees, Loretta Lynn, Adult, N.W.A. and Gary Numan all groove within Anita's magic sonic sofrito!",
+        "This mix is quintessential Anita Drink at her best! Recorded for San Francisco's legendary Built for Speed party at Delirium in the Mission at 16 & Valencia. This Thursday weekly ran for 11 years! featuring tracks inspired and played by her co-founders DJ sisters Platform Whore & Fashion Hooker. Get ready to strap in for a wild ride! Featuring tracks by The Monkees, Loretta Lynn, Adult, N.W.A. and Gary Numan all groove within Anita's magic sonic sofrito!",
     },
     {
       alt: 'Anita posing with hands on hips',
       imageSrc: '/anita-hip-pose.webp',
       playerSrc:
         'https://player-widget.mixcloud.com/widget/iframe/?hide_cover=1&light=1&feed=%2Fanitadrink%2Fanita-drink-eye-am-the-dj-mixtape%2F',
-      date: '12.01.99',
+      date: '12.01.99 San Francisco',
       description:
-        "Anita Drink's Eye Am The DJ Mixtape.Anita unchained! And coming hot! All vinyl sound track of punk, rock and trash! Recorded live! At a house-party birthday in San Francisco on two turntables with attitude! A drive thru of adventure! Starring the likes of Motörhead, The Doors, U.F.O and the Smiths",
+        "Anita Drink's Eye Am The DJ Mixtape. Anita unchained! And coming hot! All vinyl sound track of punk, rock and trash! Recorded live! At a house-party birthday in San Francisco on two turntables with attitude! A drive thru of adventure! Starring the likes of Motörhead, The Doors, U.F.O and the Smiths",
     },
     {
       alt: 'Anita posing pink party streamers',
@@ -58,16 +58,16 @@ export default function Home() {
       imageSrc: '/anita-red-dress.webp',
       playerSrc:
         'https://player-widget.mixcloud.com/widget/iframe/?hide_cover=1&light=1&feed=%2Fanitadrink%2Fanita-drink-presents-16th-valencia-mix%2F',
-      date: '25.05.02',
+      date: '25.05.02 Built For Speed',
       description:
-        'A slinky late-night in the Mission District at Built For Speed On the turn tables Anita Drink drops her vinyl treasure on a hungry dance-floor of devotees waiting to twist away the blues with gems from Anna Domino, Nicodemus, Jayne County Legowelt and Loleatta Holloway.',
+        'A slinky late-night in the Mission District at Built For Speed. On the turn tables Anita Drink drops her vinyl treasure on a hungry dance-floor of devotees waiting to twist away the blues with gems from Anna Domino, Nicodemus, Jayne County Legowelt and Loleatta Holloway.',
     },
     {
       alt: 'Anita pulling messy blonde hair',
       imageSrc: '/anita-hair-pull.webp',
       playerSrc:
         'https://player-widget.mixcloud.com/widget/iframe/?hide_cover=1&light=1&feed=%2Fanitadrink%2Fanita-drink-presents-berlin-mambo-part-1%2F',
-      date: '27.01.16',
+      date: '27.01.16 Das Hotel',
       description:
         "Recording Live at one of Anita's D.J. Residency at Berlin's infamous Das Hotel This mix is a special two-part adventure diving deep into underground disco, house and garage classics! Get ready to move with Chemise, Fela Kuti, the Residents and Adonis just to get the party started.",
     },
@@ -76,7 +76,7 @@ export default function Home() {
       imageSrc: '/anita-glamour.webp',
       playerSrc:
         'https://player-widget.mixcloud.com/widget/iframe/?hide_cover=1&light=1&feed=%2Fanitadrink%2Fanita-drink-presents-berlin-mambo-part-2%2F',
-      date: '14.02.17',
+      date: '14.02.17 Berlin Mambo',
       description:
         "Anita Drink's Berlin Mambo Part 2. A special two-part adventure diving deep into underground disco, house and garage classics! Get ready to move with Chemise, Fela Kuti, the Residents and Adonis just to get the party started.",
     },
@@ -85,7 +85,7 @@ export default function Home() {
       imageSrc: '/anita-makeup.webp',
       playerSrc:
         'https://player-widget.mixcloud.com/widget/iframe/?hide_cover=1&light=1&feed=%2Fanitadrink%2Fanita-drink-presents-soul-train%2F',
-      date: '13.06.12',
+      date: '13.06.12 White Trash',
       description:
         "All aboard!! Anita Drink driving us down the tracks into a passion play of live vinyl packed with soulful jams recorded live at White Trash Fast Food, a legendary Berlin haunt that was home to some of the city's amazing talent and DJ's. Expect the unexpected in this mix featuring the sounds of Bowie, Elvis, Ladytron, Bim Sherman and the Dead Boys.",
     },
@@ -94,7 +94,7 @@ export default function Home() {
       imageSrc: '/anita-ladder.webp',
       playerSrc:
         'https://player-widget.mixcloud.com/widget/iframe/?hide_cover=1&light=1&feed=%2Fanitadrink%2Fanita-drink-presents-boys-who-are-girls-mix%2F',
-      date: '2018',
+      date: '2018 Rodeo club',
       description:
         'Recorded at Rodeo club, Berlin. Turn up the volume! Lights! Action! Anita! Hot grooves fill the air with soul, disco, rare groove and modern classics that keep the fever going on the dance floor! Featuring tracks by Scissor Sisters, Blondie, Massive Attack and Joyce Sims.',
     },
