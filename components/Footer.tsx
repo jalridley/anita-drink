d10ddb75695d83f47447d7a8bc129c5f2016041f
Jal Ridley <72085091+jalridley@users.noreply.github.com>
2025-09-14 17:54:48 +0200

    created socials array for header and footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -6,6 +6,29 @@ import {
   FaBandcamp,
 } from 'react-icons/fa6';
 
+const socials = [
+  {
+    href: 'https://www.mixcloud.com/anitadrink/',
+    label: 'Mixcloud',
+    icon: FaMixcloud,
+  },
+  {
+    href: 'https://www.instagram.com/anitadrinklipstick/',
+    label: 'Instagram',
+    icon: FaInstagram,
+  },
+  {
+    href: 'https://www.facebook.com/djanitadrink/',
+    label: 'Facebook',
+    icon: FaFacebook,
+  },
+  {
+    href: 'https://eatlipstick.bandcamp.com/',
+    label: 'Bandcamp',
+    icon: FaBandcamp,
+  },
+];
+
 const Footer = () => {
   return (
     <footer className="-top-14 flex h-8 w-full shrink-0 items-center justify-between gap-2 bg-black px-4 py-6 text-xs font-extrabold text-white sm:flex-row md:text-sm lg:px-6">
@@ -13,21 +36,21 @@ const Footer = () => {
         ©{new Date().getFullYear()} Anita Drink. All rights reserved.
       </p>
       <nav className="flex h-8 w-full justify-center gap-6 sm:gap-8 md:justify-end">
-        <Link href={'https://www.mixcloud.com/anitadrink/'} target="_blank">
-          <FaMixcloud className="h-full w-10" />
-        </Link>
-        <Link
-          href={'https://www.instagram.com/anitadrinklipstick/'}
-          target="_blank"
-        >
-          <FaInstagram className="h-full w-full" />
-        </Link>
-        <Link href={'https://www.facebook.com/djanitadrink/'} target="_blank">
-          <FaFacebook className="h-full w-full" />
-        </Link>
-        <Link href={'https://eatlipstick.bandcamp.com/'} target="_blank">
-          <FaBandcamp className="h-full w-full" />
-        </Link>
+        {socials.map(({ href, label, icon: Icon }) => (
+          <Link
+            key={label}
+            href={href}
+            target="_blank"
+            aria-label={label}
+            className="flex h-full w-8 items-center justify-center hover:text-cyan-400 md:w-10"
+          >
+            {label === 'Mixcloud' ? (
+              <Icon className="h-full w-10" />
+            ) : (
+              <Icon className="h-full w-full" />
+            )}
+          </Link>
+        ))}
       </nav>
     </footer>
   );
--- components/Header.tsx
+++ components/Header.tsx
@@ -1,5 +1,6 @@
 import Link from 'next/link';
 import Image from 'next/image';
+import { twMerge } from 'tailwind-merge';
 
 import {
   FaMixcloud,
@@ -20,34 +21,85 @@ const Header = () => {
     },
     { label: 'bookings', url: '/bookings' },
   ];
+
+  const socials = [
+    {
+      href: 'https://www.mixcloud.com/anitadrink/',
+      label: 'Mixcloud',
+      icon: FaMixcloud,
+    },
+    {
+      href: 'https://www.instagram.com/anitadrinklipstick/',
+      label: 'Instagram',
+      icon: FaInstagram,
+    },
+    {
+      href: 'https://www.facebook.com/djanitadrink/',
+      label: 'Facebook',
+      icon: FaFacebook,
+    },
+    {
+      href: 'https://eatlipstick.bandcamp.com/',
+      label: 'Bandcamp',
+      icon: FaBandcamp,
+    },
+  ];
+
   return (
     <header className="fixed left-0 right-0 top-0 z-50 flex h-14 items-center justify-between bg-pink-600 px-2 py-2 text-lg font-extrabold text-white shadow-xl md:px-4 md:text-xl lg:space-x-4 lg:px-6 lg:text-3xl">
-      <Link className="flex h-full items-center justify-center" href="/">
+      <Link
+        className="flex h-full items-center justify-center"
+        href="/"
+        aria-label="Home"
+      >
         <Image
-          className="mr-2 h-full w-auto rounded-full border border-black"
+          className="mr-2 h-full w-auto rounded-full border border-black hover:border-2 hover:border-cyan-400"
           alt="Anita face stencil avatar"
           src={'/anita-stencil-thumb.webp'}
           width={272}
           height={272}
         />
-        <span className="shrink-0">Anita Drink</span>
+        <span className="shrink-0 hover:text-cyan-400">Anita Drink</span>
       </Link>
       <div className="flex shrink-0 items-center gap-4 lg:gap-6">
-        <Link href={'https://www.mixcloud.com/anitadrink/'} target="_blank">
-          <FaMixcloud className="flex h-full w-8 shrink-0 md:w-10" />
+        {socials.map(({ href, label, icon: Icon }) => (
+          <Link key={label} href={href} target="_blank" aria-label={label}>
+            <Icon
+              className={twMerge(
+                'flex h-full w-full shrink-0 hover:text-cyan-400',
+                label === 'Mixcloud' ? 'w-8 md:w-10' : '',
+              )}
+            />
+          </Link>
+        ))}
+        {/* <Link
+          href={'https://www.mixcloud.com/anitadrink/'}
+          target="_blank"
+          aria-label="Mixcloud"
+        >
+          <FaMixcloud className="flex h-full w-8 shrink-0 hover:text-cyan-400 md:w-10" />
         </Link>
         <Link
           href={'https://www.instagram.com/anitadrinklipstick/'}
           target="_blank"
+          aria-label="Instagram"
         >
-          <FaInstagram className="flex h-full w-full shrink-0" />
+          <FaInstagram className="flex h-full w-full shrink-0 hover:text-cyan-400" />
         </Link>
-        <Link href={'https://www.facebook.com/djanitadrink/'} target="_blank">
-          <FaFacebook className="flex h-full w-full shrink-0" />
+        <Link
+          href={'https://www.facebook.com/djanitadrink/'}
+          target="_blank"
+          aria-label="Facebook"
+        >
+          <FaFacebook className="flex h-full w-full shrink-0 hover:text-cyan-400" />
         </Link>
-        <Link href={'https://eatlipstick.bandcamp.com/'} target="_blank">
-          <FaBandcamp className="flex h-full w-full shrink-0" />
-        </Link>
+        <Link
+          href={'https://eatlipstick.bandcamp.com/'}
+          target="_blank"
+          aria-label="Bandcamp"
+        >
+          <FaBandcamp className="flex h-full w-full shrink-0 hover:text-cyan-400" />
+        </Link> */}
 
         <nav
           aria-label="Main Navigation"
@@ -58,7 +110,7 @@ const Header = () => {
               <Button
                 key={item.label}
                 asChild
-                className="rounded-lg bg-cyan-400 font-extrabold md:text-xl lg:text-3xl"
+                className="rounded-lg bg-cyan-400 font-extrabold text-gray-800 hover:text-white md:text-xl lg:text-3xl"
                 size="lg"
                 variant="default"
               >
@@ -71,7 +123,12 @@ const Header = () => {
                 </Link>
               </Button>
             ) : (
-              <Link key={item.label} href={item.url} target={item.target}>
+              <Link
+                key={item.label}
+                href={item.url}
+                target={item.target}
+                className="hover:text-cyan-400"
+              >
                 {item.label}
               </Link>
             ),
